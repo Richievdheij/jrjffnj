7636dbebdcefdcdbad946ccaf51ab795439b0b45
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2022-05-10 13:41:25 +0200

    opdr leraar

---
+++ opd1.py
@@ -0,0 +1,3 @@
+fruitmand = ['peer', 'ananas', 'appel', 'sinaasappel', 'druif', 'meloen']
+fruit = fruitmand [2]
+print(fruit)
---
+++ opdr2.py
@@ -0,0 +1,3 @@
+fruitmand = ['peer', 'ananas', 'appel', 'sinaasappel', 'druif', 'meloen']
+for i in fruitmand:
+  print(i)
